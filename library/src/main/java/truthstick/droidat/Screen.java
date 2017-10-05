13435608a30308fa7f08428fd18d9baa66725dc3
Benjamin P Lee <benjaminplee@gmail.com>
2017-10-04 21:16:49 -0400

    Porting more over from past work

--- library/src/main/java/truthstick/droidat/Screen.java
+++ library/src/main/java/truthstick/droidat/Screen.java
@@ -3,16 +3,28 @@ package truthstick.droidat;
 import android.app.Activity;
 import android.app.Fragment;
 
-public class Screen {
-    private final Class<? extends Activity> screenUnderTest;
-    private final Class<? extends Fragment> fragmentUnderTest;
+public abstract class Screen {
+    private final Class<? extends Activity> expectedTopActivity;
+    private final Class<? extends Fragment> expectedTopFragment;
 
-    public Screen(Class<? extends Activity> screenUnderTest) {
-        this(screenUnderTest, null);
+    public Screen(Class<? extends Activity> expectedTopActivity) {
+        this(expectedTopActivity, null);
     }
 
-    public Screen(Class<? extends Activity> screenUnderTest, Class<? extends Fragment> fragmentUnderTest) {
-        this.screenUnderTest = screenUnderTest;
-        this.fragmentUnderTest = fragmentUnderTest;
+    public Screen(Class<? extends Activity> expectedTopActivity, Class<? extends Fragment> expectedTopFragment) {
+        this.expectedTopActivity = expectedTopActivity;
+        this.expectedTopFragment = expectedTopFragment;
+    }
+
+    public Class<? extends Activity> getExpectedTopActivity() {
+        return expectedTopActivity;
+    }
+
+    public Class<? extends Fragment> getExpectedTopFragment() {
+        return expectedTopFragment;
+    }
+
+    public boolean expectsFragment() {
+        return expectedTopFragment != null;
     }
 }
